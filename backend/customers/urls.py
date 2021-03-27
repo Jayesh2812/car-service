cb90c14a96f3f2bf5aa18498dac2ede0ad2a3272
Jayesh Deorukhkar <52153715+Jayesh2812@users.noreply.github.com>
2021-03-28 03:13:10 +0530

    ServiceCenter Authentication complete

--- backend/customers/urls.py
+++ backend/customers/urls.py
@@ -12,5 +12,5 @@ urlpatterns = [
     path('login', login_view, name="login"),
     path('signup', signup_view, name="signup"),
     path('logout', logout_view, name="logout"),
-    path('home', home_view, name="home"),
+    path('', home_view, name="home"),
 ]
--- backend/service_centers/models.py
+++ backend/service_centers/models.py
@@ -8,17 +8,20 @@ from customers.models import Customer
 class ServiceCenter(models.Model):
     user=models.OneToOneField(User,on_delete=models.CASCADE)
     phoneNo=models.DecimalField(max_digits=10,decimal_places=0)
-    centerName=models.CharField(max_length=300)
-    openingTime=models.TimeField()
-    closingTime=models.TimeField()
-    locationState=models.CharField(max_length=200)
-    locationCity=models.CharField(max_length=200)
-    locationPinCode=models.DecimalField(max_digits=6,decimal_places=0)
-    address=models.CharField(max_length=500)
-    numberOfMechs=models.DecimalField(max_digits=4,decimal_places=0)
-    baseVisitingCharges=models.DecimalField(max_digits=4,decimal_places=2)
-    availableServices= models.CharField(max_length=800)         #along with type of service
+    centerName=models.CharField(max_length=300, null = True, blank = True)
+    openingTime=models.TimeField( null = True, blank = True)
+    closingTime=models.TimeField( null = True, blank = True)
+    locationState=models.CharField(max_length=200, null = True, blank = True)
+    locationCity=models.CharField(max_length=200, null = True, blank = True)
+    locationPinCode=models.DecimalField(max_digits=6,decimal_places=0, null = True, blank = True)
+    address=models.CharField(max_length=500, null = True, blank = True)
+    numberOfMechs=models.DecimalField(max_digits=4,decimal_places=0, null = True, blank = True)
+    baseVisitingCharges=models.DecimalField(max_digits=4,decimal_places=0, null = True, blank = True)
+    availableServices= models.CharField(max_length=800, null = True, blank = True)         #along with type of service
 
 
+    def __str__(self):
+        return f"{self.user}"
+    
 
 #Bills
