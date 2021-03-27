14673c0973c77ce47549a1dfb271a145c9fcf9ac
Jayesh Deorukhkar <52153715+Jayesh2812@users.noreply.github.com>
2021-03-27 21:41:59 +0530

    Customers Login and signin

--- backend/car_service_api/urls.py
+++ backend/car_service_api/urls.py
@@ -20,7 +20,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('customer/', include('customers.urls'))
-]
     path('service_center/',include('service_centers.urls'))
+]
 
 
